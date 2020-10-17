c462839fda818890aaad8cc24a71f9e727ed579d
akky-xxxx <114.s.aki@gmail.com>
2020-10-17 13:31:36 +0900

    chore: setup development infra

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const OFF = 0
+const WARN = 1
+const ERROR = 2
+/* eslint-enable @typescript-eslint/no-unused-vars */
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "airbnb-base",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "no-nested-ternary": OFF,
+    "import/prefer-default-export": OFF,
+    "import/extensions": [
+      "error",
+      {
+        js: "never",
+        ts: "never",
+      },
+    ],
+    "prettier/prettier": ERROR,
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".ts"],
+      },
+    },
+  },
+}
