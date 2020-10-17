afb87371641241b06b7084bf684437cfa89e98e8
akky-xxxx <114.s.aki@gmail.com>
2020-10-17 19:56:34 +0900

    feat: define const of endpoint

---
+++ src/const/Server/Endpoint/index.ts
@@ -0,0 +1,10 @@
+// main
+export const Endpoint = {
+  ROOT: "/",
+  TEST: {
+    PLUGIN1: "/test/plugin1",
+    PLUGIN1_2: "/test/plugin1-2",
+    PLUGIN1_ID: "/test/plugin1/:id",
+    CONTROLLER: "/test/controller1",
+  },
+} as const
