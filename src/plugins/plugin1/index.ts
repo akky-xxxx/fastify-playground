10ae338ae770eb9f923bf77c7d7c638e3f2f6746
akky-xxxx <114.s.aki@gmail.com>
2020-10-17 14:31:47 +0900

    feat: implements hooks to plugin1

--- src/plugins/plugin1/index.ts
+++ src/plugins/plugin1/index.ts
@@ -3,6 +3,27 @@ import { FastifyPluginAsync } from "fastify"
 
 // main
 export const plugin1: FastifyPluginAsync = async (fastify) => {
+  fastify.addHook("onRequest", async () => {
+    console.log("/plugin1 - onRequest")
+  })
+  fastify.addHook("preParsing", async (_req, _rep, payload) => {
+    console.log("/plugin1 - preParsing")
+    return payload
+  })
+  fastify.addHook("preHandler", async () => {
+    console.log("/plugin1 - preHandler")
+  })
+  fastify.addHook("onError", async (_req, _rep, error) => {
+    console.log("/plugin1 - onError")
+    console.error(error)
+  })
+  fastify.addHook("onSend", async (_req, _rep, payload) => {
+    console.log("/plugin1 - onSend")
+    return payload
+  })
+  fastify.addHook("onResponse", async () => {
+    console.log("/plugin1 - onResponse")
+  })
   fastify.get("/plugin1", (_req, reply) => {
     reply.send("/plugin1")
   })
