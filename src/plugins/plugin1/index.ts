4ba81d9ba2d17074d69461c24f92cf04b2b19a9c
akky-xxxx <114.s.aki@gmail.com>
2020-10-17 14:08:45 +0900

    feat: increase plugin1-2

--- src/plugins/plugin1/index.ts
+++ src/plugins/plugin1/index.ts
@@ -6,4 +6,7 @@ export const plugin1: FastifyPluginAsync = async (fastify) => {
   fastify.get("/plugin1", (_req, reply) => {
     reply.send("/plugin1")
   })
+  fastify.get("/plugin1-2", async () => {
+    return "/plugin1-2"
+  })
 }
