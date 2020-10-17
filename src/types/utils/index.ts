2545b5d10d33cc55393667bc28be5aab47fa0e4b
akky-xxxx <114.s.aki@gmail.com>
2020-10-17 23:02:31 +0900

    feat: define StringObject and exchange it

--- src/types/utils/index.ts
+++ src/types/utils/index.ts
@@ -2,23 +2,25 @@
 import { AnyObject } from "../common"
 
 // main
+type StringObject = Record<string, string>
+
 /**
  * @typedef CreatePartialQuery
  * @description request handler の query 定義用。未指定も可能なので partial とする。query なので value の型は全て string になる
- * @param T - extends Record<string, string>
+ * @param T - extends StringObject
  * @property Querystring - optional T
  */
-export type CreatePartialQuery<T extends Record<string, string>> = {
+export type CreatePartialQuery<T extends StringObject> = {
   Querystring?: Partial<T>
 }
 
 /**
  * @typedef CreateRequireQuery
  * @description request handler の query 定義用。query なので value の型は全て string になる
- * @param T - extends Record<string, string>
+ * @param T - extends StringObject
  * @property Querystring - optional T
  */
-export type CreateRequireQuery<T extends Record<string, string>> = {
+export type CreateRequireQuery<T extends StringObject> = {
   Querystring?: T
 }
 
