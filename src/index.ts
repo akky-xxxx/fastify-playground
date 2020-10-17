7d579635de81984a7ecf9e3d99587e2f9e9641e6
akky-xxxx <114.s.aki@gmail.com>
2020-10-17 19:48:39 +0900

    feat: display routes when dev

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,25 @@ import { controller1 } from "./controllers/controller1"
 // import others
 import { fastify } from "./modules/fastify"
 import { Server } from "./const/Server"
+import { Common } from "./const/Common"
 
 // main
 const { PORT } = Server
+const { IS_DEV } = Common
 
 fastify.register(plugin1)
 fastify.get("/", async (_req, reply) => {
   reply.send("Hello fastify world.")
 })
 fastify.get("/controller1", controller1)
+
+fastify.ready(() => {
+  if (IS_DEV) {
+    console.log("//////////////////////////////////////////////////////////////////////")
+    console.log(fastify.printRoutes())
+    console.log("//////////////////////////////////////////////////////////////////////")
+  }
+})
 
 fastify.listen(PORT, (error, address) => {
   if (error) {
