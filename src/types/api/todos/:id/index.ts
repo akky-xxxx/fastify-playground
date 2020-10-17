379f5246bfe1737f9061d0dc488364fb1351d989
akky-xxxx <114.s.aki@gmail.com>
2020-10-17 23:07:17 +0900

    refactor: add tscods to api/todos/:id

--- src/types/api/todos/:id/index.ts
+++ src/types/api/todos/:id/index.ts
@@ -9,13 +9,22 @@ import {
 // import others
 import { CreatePartialParams, CreateRequireParams } from "../../../utils"
 
+// main
 type ParamsBase = {
   id: string
 }
 
+/**
+ * @typedef TodosPartialParams
+ * @description hooks 用
+ */
 export type TodosPartialParams = CreatePartialParams<ParamsBase>
 type TodosRequireParams = CreateRequireParams<ParamsBase>
 
+/**
+ * @typedef TodosIdController
+ * @description api/todos/:id の controller 用
+ */
 export type TodosIdController = RouteHandlerMethod<
   RawServerDefault,
   RawRequestDefaultExpression,
