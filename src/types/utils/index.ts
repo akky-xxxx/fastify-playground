2554a1b6c0ae79f1f302de20e57fc044392d401a
akky-xxxx <114.s.aki@gmail.com>
2020-10-18 00:20:24 +0900

    fix: replace AnyObject to StringObject

--- src/types/utils/index.ts
+++ src/types/utils/index.ts
@@ -1,6 +1,3 @@
-// import
-import { AnyObject } from "../common"
-
 // main
 type StringObject = Record<string, string>
 
@@ -27,19 +24,19 @@ export type CreateRequireQuery<T extends StringObject> = {
 /**
  * @typedef CreateRequireParams
  * @description request handler の param 定義湯
- * @param T - extends AnyObject
+ * @param T - extends StringObject
  * @property Params - optional T
  */
-export type CreateRequireParams<T extends AnyObject> = {
+export type CreateRequireParams<T extends StringObject> = {
   Params: T
 }
 
 /**
  * @typedef CreatePartialParams
  * @description request handler の param 定義湯。未指定も可能なので partial とする
- * @param T - extends AnyObject
+ * @param T - extends StringObject
  * @property Params - optional T
  */
-export type CreatePartialParams<T extends AnyObject> = {
+export type CreatePartialParams<T extends StringObject> = {
   Params?: Partial<T>
 }
