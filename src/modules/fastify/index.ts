5fa35a43c5f421e96edfb18cbb478ae28ffb84b9
akky-xxxx <114.s.aki@gmail.com>
2020-10-17 19:30:22 +0900

    feat: handling logger by NODE_ENV

--- src/modules/fastify/index.ts
+++ src/modules/fastify/index.ts
@@ -1,7 +1,11 @@
 // import node_modules
 import fastifyOrigin from "fastify"
 
+// import const
+import { Common } from "../../const/Common"
+
 // main
+const { IS_DEV } = Common
 export const fastify = fastifyOrigin({
-  logger: true,
+  logger: IS_DEV,
 })
