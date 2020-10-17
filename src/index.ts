4513e43982add536bc3d91e10c7bb98b6320c6d7
akky-xxxx <114.s.aki@gmail.com>
2020-10-17 13:53:39 +0900

    refactor: split fastify instanse

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-// import node_modules
-import fastifyOrigin from "fastify"
-
 // import plugins
 import { plugin1 } from "./plugins/plugin1"
 
@@ -9,13 +6,11 @@ import { controller1 } from "./controllers/controller1"
 
 // import others
 import "./modules/firstImport"
+import { fastify } from "./modules/fastify"
 import { Server } from "./const/Server"
 
 // main
 const { PORT } = Server
-const fastify = fastifyOrigin({
-  logger: true,
-})
 
 fastify.register(plugin1)
 fastify.get("/", async (_req, reply) => {
---
+++ src/modules/fastify/index.ts
@@ -0,0 +1,7 @@
+// import node_modules
+import fastifyOrigin from "fastify"
+
+// main
+export const fastify = fastifyOrigin({
+  logger: true,
+})
