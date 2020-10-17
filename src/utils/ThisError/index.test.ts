4b35f7646a6f7c2b1d62930baed2088f6b897092
akky-xxxx <114.s.aki@gmail.com>
2020-10-18 01:28:46 +0900

    test: implement of ThisError

---
+++ src/utils/ThisError/index.test.ts
@@ -0,0 +1,11 @@
+// import
+import { ThisError } from "./"
+
+// main
+describe("ThisError", () => {
+  it("number 型の status と string 型の message を持つインスタンスが作成される", () => {
+    const thisError = new ThisError({ status: 400, message: "message" })
+    expect(typeof thisError.status).toEqual("number")
+    expect(typeof thisError.message).toEqual("string")
+  })
+})
