4d01f4dad81e2021b25768b60d5f9d5e54e4e458
akky-xxxx <114.s.aki@gmail.com>
2020-10-17 22:54:35 +0900

    refactor: generics extends AnyObject

--- src/types/utils/index.ts
+++ src/types/utils/index.ts
@@ -1,11 +1,15 @@
-export type CreatePartialQuery<T> = {
+// import
+import { AnyObject } from "../common"
+
+// main
+export type CreatePartialQuery<T extends AnyObject> = {
   Querystring?: Partial<T>
 }
 
-export type CreateRequireParams<T> = {
+export type CreateRequireParams<T extends AnyObject> = {
   Params: T
 }
 
-export type CreatePartialParams<T> = {
+export type CreatePartialParams<T extends AnyObject> = {
   Params?: Partial<T>
 }
