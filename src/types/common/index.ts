3d42a4a7dfc532b2c2dcb20ff045fd9d367ac796
akky-xxxx <114.s.aki@gmail.com>
2020-10-18 00:53:45 +0900

    feat: define StringObject as common

--- src/types/common/index.ts
+++ src/types/common/index.ts
@@ -3,3 +3,9 @@
  * @description object 型なら何でも可
  */
 export type AnyObject = Record<string, unknown>
+
+/**
+ * @typedef StringObject
+ * @description query や param 等、 string しか持たないオブジェクト型
+ */
+export type StringObject = Record<string, string>
