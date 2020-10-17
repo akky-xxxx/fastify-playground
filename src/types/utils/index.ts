1868c82ee1030e8193078e8f65fe81efef0cfd49
akky-xxxx <114.s.aki@gmail.com>
2020-10-18 01:10:02 +0900

    refactor: fix PartialRecord

--- src/types/utils/index.ts
+++ src/types/utils/index.ts
@@ -1,9 +1,9 @@
-// import
-import { AnyObject } from "../common"
-
 // main
 /**
  * @typedef PartialRecord
  * @description 全ての key を optional で定義する Record
+ * @param K - extends keyof any
+ * @param T - type of each keys
  */
-export type PartialRecord<K extends keyof AnyObject, T> = Partial<Record<K, T>>
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type PartialRecord<K extends keyof any, T> = Partial<Record<K, T>>
