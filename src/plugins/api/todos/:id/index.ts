14b17e9e7372e968db3f59f6432930c8c899414e
akky-xxxx <114.s.aki@gmail.com>
2020-10-18 22:59:34 +0900

    feat: connect & disconnect at plugins api/todos/:id

--- src/plugins/api/todos/:id/index.ts
+++ src/plugins/api/todos/:id/index.ts
@@ -1,5 +1,6 @@
 // import node_modules
 import { FastifyPluginAsync } from "fastify"
+import mongoose from "mongoose"
 
 // import controllers
 import { todosIdGet } from "../../../../controllers/api/todos/:id/get"
@@ -10,6 +11,7 @@ import { todosIdDelete } from "../../../../controllers/api/todos/:id/delete"
 import { Endpoint } from "../../../../const/Server/Endpoint"
 import { TodosPartialParams } from "../../../../types/api/todos/:id"
 import { ThisError } from "../../../../utils/ThisError"
+import { connectMongoose } from "../../../../utils/connectMongoose"
 
 // main
 const {
@@ -18,14 +20,23 @@ const {
 
 export const todosIdPlugin: FastifyPluginAsync = async (fastify) => {
   fastify.addHook<TodosPartialParams>("preHandler", async (req) => {
-    console.log("connect to db when preHandler")
     if (!req.params?.id) {
       throw new ThisError({ status: 400, message: "params.id がありません" })
+    }
+
+    try {
+      await connectMongoose()
+    } catch (error) {
+      throw new ThisError({ error })
     }
   })
 
   fastify.addHook("onResponse", async () => {
-    console.log("disconnect from db when onResponse")
+    try {
+      await mongoose.disconnect()
+    } catch (error) {
+      throw new ThisError({ error })
+    }
   })
 
   fastify.get(TODOS_ID, todosIdGet)
