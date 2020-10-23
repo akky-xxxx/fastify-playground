8a1d4d9818ad939a965b7da84074650ca54ad5b2
akky-xxxx <114.s.aki@gmail.com>
2020-10-23 15:00:12 +0900

    feat: change variables pug to controller

--- src/controllers/web/home/index.ts
+++ src/controllers/web/home/index.ts
@@ -1,10 +1,28 @@
 // import node_modules
 import { RouteHandlerMethod } from "fastify"
 
+// import others
+import { PageArguments } from "./types"
+
 // main
 export const home: RouteHandlerMethod = async (_req, reply) => {
-  return reply.view("/pages/home/index.pug", {
-    message: "message",
+  const pageArguments: PageArguments = {
+    todoItems: [
+      {
+        id: "1",
+        title: "todo item1",
+        description: "description1",
+        isDone: false,
+      },
+      {
+        id: "2",
+        title: "todo item2",
+        description: "",
+        isDone: true,
+      },
+    ],
     name: "Taro",
-  })
+    message: "message",
+  }
+  return reply.view("/pages/home/index.pug", pageArguments)
 }
