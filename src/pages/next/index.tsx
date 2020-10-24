b0dfe68dd9d23ebc414d51582bafce51566b9559
akky-xxxx <114.s.aki@gmail.com>
2020-10-25 01:26:37 +0900

    feat: connect to store

--- src/pages/next/index.tsx
+++ src/pages/next/index.tsx
@@ -1,12 +1,36 @@
 // import node_modules
-import React from "react"
+import React, { useEffect } from "react"
 import styled from "styled-components"
+
+import { useDispatch, useSelector } from "../../store"
+import { readTodoItems } from "../../store/reducers/todoItems"
 
 // main
 const home = () => {
+  const state = useSelector((state) => state.todoItems)
+  const dispatch = useDispatch()
+
+  const { data } = state
+
+  useEffect(() => {
+    if (!data) dispatch(readTodoItems())
+  }, [data])
+
+  if (!data) return null
+
   return (
     <div>
       <Red>next top</Red>
+      <ul>
+        {data.todoItems.map((todoItem) => {
+          const { id, title, description, isDone } = todoItem
+          return (
+            <li key={id}>
+              {isDone ? "完了" : "未完"}, {title}, {description}
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
